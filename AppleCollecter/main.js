79af349d7cda5b268aaf4596272f9b2acad999eb
Myungjin Song <reki318@naver.com>
2018-06-15 22:00:28 +0900

    add Global Variable Manager

--- AppleCollecter/main.js
+++ AppleCollecter/main.js
@@ -1,9 +1,13 @@
+var DataManager = {
+  standardLists : [],
+  customLists : []
+}
+
 var AppleModel = function AppleModel(XMLHttpRequest){
     this.XMLHttpRequest = XMLHttpRequest;
-    this.rawDataList = [];
     this.myList = [];
     this.secretToken = "b3c2e4d351ec4a56bf403743d6056888";
-    this.query = "ios";
+    this.query = "Apple";
     this.index = 0;
 }
 
@@ -11,16 +15,13 @@ AppleModel.prototype.getAppleData = function getAppleData(method,url,callback){
     var request = new this.XMLHttpRequest();
 
     request.onload = function(){
-        this.rawDataList = JSON.parse(request.responseText);
-        callback(this.rawDataList);
+        var rawDataList = JSON.parse(request.responseText);
+        callback(rawDataList);
     }
     request.open(method, url, true);
     request.send();
 }
 
-AppleModel.prototype.saveData = function saveData(){
-  console.log(this.myList)
-}
 
 var AppleView = function AppleView(element){
     this.element = element;
@@ -36,7 +37,8 @@ AppleView.prototype.render = function render(obj){
     objKeys.map(function(element){
         if (element === "urlToImage" && obj[element] !== null) {
             str += "<div class='image'>";
-            str += "<img src=" + obj[element] + " alt=" + obj[element] + "/>";
+            //console.log(obj["url"]);
+            str += "<img src=" + obj[element] + " alt=" + obj["url"] + "/>";
             str += "</div>";
         }
         str += "<div class='content'>";
@@ -47,13 +49,10 @@ AppleView.prototype.render = function render(obj){
             str += "<span class='date'>" +obj[element] + "</span>";
             str += "</div>";
         }
-
         if(element === "description"){
             str += "<div class='description'>";
             str += obj[element];
             str += "</div>";
-        }else if(element === "url"){
-            //str += "<a href=" + obj[element] + " target=_blank>Go to the Link! </a>";
         }
         str += "</div>";
     });
@@ -74,28 +73,30 @@ AppleView.prototype.render = function render(obj){
 var AppleController = function AppleController(appleModel, appleView){
     this.appleModel = appleModel;
     this.appleView = appleView;
-
 }
 
 AppleController.prototype.initialize = function initialize(){
-  console.log(this);
-    this.appleView.translate = this.translate.bind(this);//
+    this.appleView.translate = this.translate.bind(this);
     this.appleView.saveFavorite = this.saveFavorite.bind(this);
-    console.log(this.appleModel);
 }
 
 AppleController.prototype.saveFavorite = function saveFavorite(param){
     //saveFavorite
     var index = param.toElement.parentElement.parentElement.getAttribute("index");
-    console.log(param);
+    if(DataManager.customLists.includes(DataManager.standardLists.articles[index]) == false){
+      console.log("해당 데이터를 추가합니다...");
+      DataManager.customLists.push(DataManager.standardLists.articles[index]);
+    }else{
+      console.log("이미 추가 되있음..");
+    }
 
 }
 
 AppleController.prototype.translate = function translate(param){
     //Translate words
     var element = param.toElement.parentElement.parentElement;
-    let title = element.querySelector("h2").textContent;
-    let description = element.querySelector(".description").textContent;
+    let title = element.getElementsByTagName("h2")[0].textContent;
+    let description = element.getElementsByClassName("description")[0].textContent;
 
     let langTranslate = "https://bcdefence.herokuapp.com/translate/convert?";// //&from=en&to=ru&text=hello%3Cb%3E";
     let langDetect = "https://bcdefence.herokuapp.com/translate/check?text=";
@@ -119,7 +120,6 @@ AppleController.prototype.translate = function translate(param){
           }
         });
     })
-
     param.toElement.removeEventListener('click',translate); //한번만 클릭할 수 있게 제작
 }
 
@@ -142,28 +142,27 @@ AppleController.prototype.displayView = function displayView(obj){
           console.log(Exception);
         }
         if(element["urlToImage"] == null){
-            param["urlToImage"] = "https://www.elegantthemes.com/blog/wp-content/uploads/2017/07/404-error.png";
-        }/*
-        else if(element["author"] === null){
-            param["author"] = "unknown..";
-        }*/
-
+            param["urlToImage"] = "https://newsclick.in/themes/newsclick/assets/images/newsclick_logo.png";
+        }
         articleIndex++;
         params.push(param);
     });
 
     for(var i=0;i<params.length;i++){
         this.appleView.render(params[i]);
+        console.log("Draw Call..."+i);
     }
 
     var allElements = this.appleView.element.querySelectorAll("button[val=translate]");
     for(var  i = 0;i<allElements.length;i++){
-        //console.log(allElements[i].parentElement.querySelector("button[val=save]"));
         allElements[i].parentElement.querySelector("button[val=save]").addEventListener('click',this.saveFavorite);
-
+        allElements[i].parentElement.parentElement.querySelector(".image").children[0].addEventListener('click',openUrl);
         allElements[i].addEventListener('click',this.translate); //give all elements eventListener
+
     }
 
+    console.log("start initialize...");
+    this.initialize();
 }
 
 function HttpRequest(method, url ,callback){
@@ -174,19 +173,30 @@ function HttpRequest(method, url ,callback){
     request.open(method, url, true);
     request.send();
 }
-
-function saveListData(appleModel){
-  console.log(appleModel.myList);
-  //localStorage.setItem("myFavoriteList",this.myList);
+//act like a taget _blank
+function openUrl(params){
+  window.alert("해당 기사 페이지로 이동합니다!");
+  window.open(params.toElement.getAttribute('alt'));
 }
 
-function loadListData(){
-  return localStorage.getItem("myList");
+function BrowserCheck(){
+  var agent = navigator.userAgent.toLowerCase();
+  if ( (navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
+     // ie일 경우
+     window.alert("본 SPA는 크롬 브라우저에 최적화 되어있습니다.");
+   }
 }
 
-var saveManager = {
-  mylist : []
-};
+function openNavigation(){
+  document.getElementById("nav").style.width = "250px";
+}
+
+function closeNavigation(){
+  document.getElementById("nav").style.width = "0px";
+}
+
+
+BrowserCheck();
 
 window.onload = function(){
     var appleView = new AppleView(document.getElementById("content"));
@@ -198,9 +208,10 @@ window.onload = function(){
       "https://newsapi.org/v2/everything?q='"+ appleModel.query +"'&sortBy=publishedAt&apiKey="+appleModel.secretToken,
       function(list) {
         appleModel.myList = list;
+        DataManager.standardLists = appleModel.myList;
         appleController = new AppleController(appleModel, appleView);
-        appleController.initialize();
         appleController.displayView(appleModel.myList);
+        //appleController.initialize();
       }
     );
 }
